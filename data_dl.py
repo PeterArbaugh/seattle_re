991631c3084e09079c64d3a2dc5766389a00a438
PeterArbaugh <PeterArbaugh@users.noreply.github.com>
2022-04-30 11:17:27 -0700

    add fake_useragent

--- data_dl.py
+++ data_dl.py
@@ -1,16 +1,21 @@
 import requests
+from fake_useragent import UserAgent
 from datetime import date
 
-download_url = 'https://www.redfin.com/stingray/api/gis-csv?al=1&include_pending_homes=false&isRentals=false&max_num_beds=4&max_price=1000000&num_baths=1.5&num_beds=2&num_homes=350&ord=redfin-recommended-asc&page_number=1&region_id=17887,16163&region_type=6,6&sf=1,2,5,6,7&status=1&uipt=1,2,3&v=8'
+ua = UserAgent()
+header = {'User-Agent':str(ua.random)}
 
-def download_file(url, filename=''):
+
+download_url = 'https://www.redfin.com/stingray/api/gis-csv?al=1&include_pending_homes=true&isRentals=false&market=seattle&max_price=1000000&num_homes=350&ord=redfin-recommended-asc&page_number=1&region_id=17887,16163&region_type=6,6&sf=1,2,5,6,7&status=131&time_on_market_range=1-&uipt=1,2,3&v=8'
+
+def download_file(url, header, filename=''):
     try:
         if filename:
             pass            
         else:
             filename = str(date.today()) + '_for_sale.csv'
 
-        with requests.get(url) as req:
+        with requests.get(url, headers=header) as req:
             with open(filename, 'wb') as f:
                 for chunk in req.iter_content(chunk_size=8192):
                     if chunk:
@@ -20,6 +25,6 @@ def download_file(url, filename=''):
         print(e)
         return None
 
-download_file(download_url)
+download_file(download_url, header)
 
  
