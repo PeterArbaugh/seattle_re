702bee0ffcffb33fb01b65730251577ba77673a4
PeterArbaugh <PeterArbaugh@users.noreply.github.com>
2022-05-27 17:27:28 -0700

    start collecting price reductions

---
+++ reduced/data_dl_reduced.py
@@ -0,0 +1,30 @@
+import requests
+from fake_useragent import UserAgent
+from datetime import date
+
+ua = UserAgent()
+header = {'User-Agent':str(ua.random)}
+
+
+download_url = 'https://www.redfin.com/stingray/api/gis-csv?al=1&include_pending_homes=false&isRentals=false&market=seattle&max_price=1000000&num_homes=350&ord=redfin-recommended-asc&page_number=1&rd=1-&region_id=16163,17887&region_type=6,6&sf=1,2,5,6,7&status=1&travel_with_traffic=false&travel_within_region=false&uipt=1,2,3,4,5,6,7,8&v=8'
+
+def download_file(url, header, filename=''):
+    try:
+        if filename:
+            pass            
+        else:
+            filename = str(date.today()) + '_reduced.csv'
+
+        with requests.get(url, headers=header) as req:
+            with open(filename, 'wb') as f:
+                for chunk in req.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
+            return filename
+    except Exception as e:
+        print(e)
+        return None
+
+download_file(download_url, header)
+
+ 
